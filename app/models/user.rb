2dcfd7d5806963c5c5e04d7166b0af913cb143d0
rea1ism <104345395+rea1ism@users.noreply.github.com>
2022-06-14 15:47:27 +0900

    Userモデルへのバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,         presence: true
+  validates :familyname_kanji, presence: true
+  validates :firstname_kanji,  presence: true
+  validates :familyname_kana,  presence: true
+  validates :firstname_kana,   presence: true
+  validates :birthday,         presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: {with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
+
 end
