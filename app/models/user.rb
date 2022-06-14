8ff042f77984becdb57bfbcfa9413cf6fe3efb39
rea1ism <104345395+rea1ism@users.noreply.github.com>
2022-06-14 16:58:47 +0900

    パスワードのバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,6 @@ class User < ApplicationRecord
   validates :birthday,         presence: true
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: {with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
 
 end
