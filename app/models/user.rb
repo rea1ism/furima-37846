3f9b00ba9a1aeae4771ebe5d25036d845a40f11f
rea1ism <104345395+rea1ism@users.noreply.github.com>
2022-06-15 12:09:20 +0900

    漢字、カタカナ制約追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,         presence: true
-  validates :familyname_kanji, presence: true
-  validates :firstname_kanji,  presence: true
-  validates :familyname_kana,  presence: true
-  validates :firstname_kana,   presence: true
+  validates :familyname_kanji, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
+  validates :firstname_kanji,  presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
+  validates :familyname_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :firstname_kana,   presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday,         presence: true
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 
 end
