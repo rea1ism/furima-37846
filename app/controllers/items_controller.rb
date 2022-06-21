1f1fab966293de33a0fa93d7e125139aac347f34
rea1ism <104345395+rea1ism@users.noreply.github.com>
2022-06-21 17:32:29 +0900

    出品情報のバリデーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,9 +8,24 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def move_to_index
     unless user_signed_in?
       redirect_to new_user_session_path
     end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :description, :category_id, :status_id, :transportation_cost_id, :prefecture_id, :leadtime_id, :price)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,15 @@
 class Item < ApplicationRecord
   validates :item_name,              presence: true
   validates :description,            presence: true
-  validates :category_id,            presence: true
+  validates :category_id,            numericality: { other_than: 1 }
   validates :status_id,              presence: true
   validates :transportation_cost_id, presence: true
   validates :prefecture_id,          presence: true
   validates :leadtime_id,            presence: true
-  validates :price,                  presence: true
+  validates :price,                  presence: true, numericality: { in: 300..9999999}, format: { with: /\A[0-9]+\z/ }
 
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :status, :transportation_cost, :prefecture
 end
