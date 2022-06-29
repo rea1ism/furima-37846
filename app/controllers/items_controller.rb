52e343b6a18d6ea3625f3615c016c22113e0e34f
rea1ism <104345395+rea1ism@users.noreply.github.com>
2022-06-29 15:25:25 +0900

    レビュー結果の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,4 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, only: [:new]
-
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -23,12 +21,6 @@ class ItemsController < ApplicationController
   end
 
   private
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to new_user_session_path
-    end
-  end
   
   def item_params
     params.require(:item).permit(:item_name, :description, :category_id, :status_id, :transportation_cost_id, :prefecture_id, :leadtime_id, :price, :image).merge(user_id: current_user.id)
