706adc00973fa29a4ff1afa171d16d2593371920
rea1ism <104345395+rea1ism@users.noreply.github.com>
2022-06-30 15:30:28 +0900

    Rubocopによる微修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update]
-  
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -38,14 +37,14 @@ class ItemsController < ApplicationController
   end
 
   private
-  
+
   def item_params
     params.require(:item).permit(:item_name, :description, :category_id, :status_id, :transportation_cost_id, :prefecture_id, :leadtime_id, :price, :image).merge(user_id: current_user.id)
   end
 
   def move_to_index
     @item = Item.find(params[:id])
-    if current_user != @item.user
+    unless current_user == @item.user
       redirect_to action: :index
     end
   end
