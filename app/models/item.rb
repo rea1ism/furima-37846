1694e2699cead3c22cbeb02284efb45983c22a5b
rea1ism <104345395+rea1ism@users.noreply.github.com>
2022-09-19 17:54:55 +0900

    投稿枚数に制限を加える

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
   has_one :purchase
   has_many_attached :images
 
-  validates :images, presence: true
+  validates :images, length: { minimum: 1, maximum: 5, message: "は1枚以上5枚以下にしてください" }
   
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
